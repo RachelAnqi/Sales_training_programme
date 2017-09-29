50c8d0b07a8bca63ca749fbb9baad31217b46d0a
RachelAnqi <rachel_anqi@163.com>
2017-09-29 19:56:45 +0800

    9.29

---
+++ server6/ui.R
@@ -0,0 +1,30 @@
+library(shiny)
+library(shinydashboard)
+library(shinyjs)
+library(DT)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(digest)
+
+
+
+
+
+
+num_fails_to_lockout<-10
+
+
+header <- dashboardHeader(
+  title = "区域管理培训模拟平台",
+  tags$li(class = "dropdown",
+          tags$li(class = "dropdown-toggle", disabled(actionLink(inputId = "save","保存"))),
+          tags$li(class = "dropdown-toggle", actionLink(inputId = "exit","退出")))
+)
+
+sidebar <- dashboardSidebar(uiOutput("sidebarpanel"))
+
+
+body <- dashboardBody(uiOutput("body"))
+
+ui <- dashboardPage(header, sidebar, body)
